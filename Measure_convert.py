09f60e0944bd048140d76c973b120d0beb7b6057
E-Ozdemir <daviddeveen89@gmail.com>
2021-03-25 18:04:52 +0100

    This folder includes my Python homeworks

---
+++ Measure_convert.py
@@ -0,0 +1,5 @@
+# Measure Converter
+# Task 1
+cel_value = float(input('Write the Celsius value you want to be converted to Fahrenheit:'))
+fahr_value = (cel_value * 9 / 5) + 32
+print(cel_value,'°C =',fahr_value,'°F' )
---
+++ Measure_convert_2.py
@@ -0,0 +1,5 @@
+# Measure Converter
+# Task 2
+km_value = float(input('Write the Kilometer value you want to be converted to Mile:'))
+mile_value = km_value / 1.609
+print(km_value,'km =', round(mile_value, 4),'Mile' )
